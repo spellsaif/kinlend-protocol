30dcd4f7007fc19beae6ce916e0fedeba1d650e3
spellsaif <this.saif.email@gmail.com>
2025-02-21 15:29:34 +0530

    added: LiquidateLoan context

---
+++ programs/kinlend-protocol/src/contexts/liquidate_loan.rs
@@ -0,0 +1,46 @@
+use anchor_lang::prelude::*;
+
+use crate::state::{CollateralVaultState, LoanRegistryState, ProtocolVaultState};
+
+#[derive(Accounts)]
+pub struct LiquidateLoan<'info> {
+
+    //signers
+    #[account(mut)]
+    pub borrower: Signer<'info>,
+
+    #[account(mut)]
+    pub lender: Signer<'info>,
+
+    #[account(
+        mut,
+        close = borrower
+    )]
+    pub loan_request: Box<Account<'info, LoanRegistryState>>,
+
+    #[account(
+        mut,
+        seeds = [b"collateral_vault", loan_request.key().as_ref()],
+        bump = collateral_vault.bump
+    )]
+    pub collateral_vault: Box<Account<'info, CollateralVaultState>>,
+
+    #[account(
+        mut,
+        seeds = [b"loan_registry"],
+        bump 
+    )]
+    pub loan_registry: Box<Account<'info, LoanRegistryState>>,
+
+    #[account(
+        mut,
+        seeds = [b"protocol_vault"],
+        bump = protocol_vault.bump
+    )]
+    pub protocol_vault: Box<Account<'info, ProtocolVaultState>>,
+
+    //program
+    pub system_program: Program<'info, System>,
+
+    
+}
--- programs/kinlend-protocol/src/contexts/mod.rs
+++ programs/kinlend-protocol/src/contexts/mod.rs
@@ -4,6 +4,7 @@ pub mod fund_loan;
 pub mod repay_loan;
 pub mod claim_collateral;
 pub mod cancel_loan_request;
+pub mod liquidate_loan;
 
 pub use create_loan_request::*;
 pub use create_protocol_vault::*;
@@ -11,3 +12,4 @@ pub use fund_loan::*;
 pub use repay_loan::*;
 pub use claim_collateral::*;
 pub use cancel_loan_request::*;
+pub use liquidate_loan::*;
