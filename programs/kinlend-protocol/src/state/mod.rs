1767e123e5b681bc44dc33983228e89310729995
spellsaif <this.saif.email@gmail.com>
2025-02-21 11:09:24 +0530

    rename: refactoring

--- programs/kinlend-protocol/src/state/mod.rs
+++ programs/kinlend-protocol/src/state/mod.rs
@@ -1,7 +1,7 @@
 pub mod loan_request;
-pub mod vault;
+pub mod collateral_vault;
 pub mod protocol_vault;
 
 pub use loan_request::*;
-pub use vault::*;
+pub use collateral_vault::*;
 pub use protocol_vault::*;
