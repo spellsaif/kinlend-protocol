257edc377d8aef8a9be9244abe6742332553a7f7
spellsaif <this.saif.email@gmail.com>
2025-02-21 12:10:49 +0530

    added: errors enum

---
+++ programs/kinlend-protocol/src/errors.rs
@@ -0,0 +1,20 @@
+use anchor_lang::error_code;
+
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Loan is already funded.")]
+    AlreadyFunded,
+
+    #[msg("Loan Request is not funded yet.")]
+    NotFunded,
+
+    #[msg("Loan has expired.")]
+    LoanExpired,
+
+    #[msg("Loan is not expired.")]
+    LoanIsNotExpired,
+
+    #[msg("Loan Request not found in the registry.")]
+    NotFoundInRegistry
+}
