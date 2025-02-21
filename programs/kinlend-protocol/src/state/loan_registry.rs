dd4970219775da6fd41c67b769431fb55d4f5cc5
spellsaif <this.saif.email@gmail.com>
2025-02-21 18:25:21 +0530

    modified:Loan Registry

--- programs/kinlend-protocol/src/state/loan_registry.rs
+++ programs/kinlend-protocol/src/state/loan_registry.rs
@@ -2,6 +2,8 @@ use anchor_lang::prelude::*;
 
 // The LoanRegistry account is a public directory of active loan requests.
 #[account]
+#[derive(InitSpace)]
 pub struct LoanRegistryState {
+    #[max_len(10)]
     pub loan_requests: Vec<Pubkey>, // List of active loan request PDAs.
 }
