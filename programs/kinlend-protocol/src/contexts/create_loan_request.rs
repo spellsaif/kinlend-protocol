9aa02f341400bc498d9f0a4633103eb220979699
spellsaif <this.saif.email@gmail.com>
2025-02-21 11:11:00 +0530

    added: CreateLoanRequest Context

---
+++ programs/kinlend-protocol/src/contexts/create_loan_request.rs
@@ -0,0 +1,35 @@
+use anchor_lang::prelude::*;
+
+use crate::state::{LoanRequestState, CollateralVaultState};
+
+#[derive(Accounts)]
+#[instruction(loan_id:u64)]
+pub struct CreateLoanRequest<'info> {
+
+    //Borrower is the one who creates the loan request account so, he/she will be paying for account creation.
+    #[account(mut)]
+    borrower: Signer<'info>,
+
+    //Creating LoanRequestState Account
+    #[account(
+        init, 
+        space = 8 + LoanRequestState::INIT_SPACE,
+        payer = borrower, 
+        seeds = [b"loan_request", borrower.key().as_ref(), &loan_id.to_le_bytes()],  //PDA for LoanRequestState Account
+        bump
+    )]
+    pub loan_request: Box<Account<'info, LoanRequestState>>,
+
+    //creating Vault account
+    #[account(
+        init,
+        payer = borrower,
+        space = 8 + CollateralVaultState::INIT_SPACE,
+        seeds = [b"loan_vault", loan_request.key().as_ref()],
+        bump
+    )]
+    pub collateral_vault: Box<Account<'info, CollateralVaultState>>,
+
+    pub system_program: Program<'info, System>,
+
+}
---
+++ programs/kinlend-protocol/src/contexts/mod.rs
@@ -0,0 +1,2 @@
+pub mod create_loan_request;
+pub use create_loan_request::*;
