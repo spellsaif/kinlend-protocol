6cacdeb1e2ead96a733f1c70d932af5b4088c6d7
spellsaif <this.saif.email@gmail.com>
2025-02-22 10:26:58 +0530

    updated: added comments

--- programs/kinlend-protocol/src/state/loan_registry_page.rs
+++ programs/kinlend-protocol/src/state/loan_registry_page.rs
@@ -5,6 +5,6 @@ use anchor_lang::prelude::*;
 pub struct LoanRegistryPageState {
 
     #[max_len(10)]
-    pub loan_requests: Vec<Pubkey>,
-    pub next_page: Option<Pubkey>
+    pub loan_requests: Vec<Pubkey>, //list of loan_request (addressess are stored)
+    pub next_page: Option<Pubkey> //pointing to next page if available
 }
