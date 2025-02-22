312ececdea856e3c210fa307e67c35722cdce8d3
spellsaif <this.saif.email@gmail.com>
2025-02-22 14:03:31 +0530

    implemented: create protcol vault

--- programs/kinlend-protocol/src/contexts/create_protocol_vault.rs
+++ programs/kinlend-protocol/src/contexts/create_protocol_vault.rs
@@ -20,3 +20,14 @@ pub struct CreateProtocolVault<'info> {
     pub system_program: Program<'info, System>
 
 }
+
+impl<'info> CreateProtocolVault<'info> {
+    pub fn create_protocol_vault(&mut self, bumps: CreateProtocolVaultBumps) -> Result<()> {
+        //creating protocol vault
+        self.protocol_vault.set_inner(ProtocolVaultState{
+            bump: bumps.protocol_vault
+        });
+
+        Ok(())
+    }
+}
