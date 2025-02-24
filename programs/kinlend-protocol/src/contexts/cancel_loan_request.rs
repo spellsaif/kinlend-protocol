bbc28c804ae3816851041833291ff9b17da9780d
spellsaif <this.saif.email@gmail.com>
2025-02-24 21:47:30 +0530

    implemented: cancel loan  request by borrower

--- programs/kinlend-protocol/src/contexts/cancel_loan_request.rs
+++ programs/kinlend-protocol/src/contexts/cancel_loan_request.rs
@@ -2,6 +2,8 @@ use anchor_lang::prelude::*;
 
 use crate::state::{CollateralVaultState, LoanRegistryState, LoanRequestState};
 
+use crate::errors::ErrorCode;
+
 #[derive(Accounts)]
 pub struct CancelLoanRequest<'info> {
     
@@ -11,6 +13,7 @@ pub struct CancelLoanRequest<'info> {
     //Loan Request
     #[account(
         mut,
+        close = borrower,
         seeds = [b"loan_request", borrower.key().as_ref(), &loan_request.loan_id.to_le_bytes()],
         bump
     )]
@@ -20,6 +23,7 @@ pub struct CancelLoanRequest<'info> {
     //Collateral Vault
     #[account(
         mut,
+        close = borrower,
         seeds = [b"collateral_vault", loan_request.key().as_ref()],
         bump = collateral_vault.bump
 
@@ -35,4 +39,33 @@ pub struct CancelLoanRequest<'info> {
 
     //program
     pub system_program: Program<'info, System>
+}
+
+impl<'info> CancelLoanRequest<'info> {
+
+    //cancel loan request
+    pub fn cancel_loan_request(&mut self) -> Result<()> {
+
+        //check loan funded or not by checking whether lender is assgined to given loan request
+        self.check_loan_funded()?;
+
+
+        //todo: implementing logic to remove loan_request from loan registry
+
+        Ok(())
+    }
+
+    //checking whether loan is funded or not
+    pub fn check_loan_funded(&mut self) -> Result<()> {
+
+        if self.loan_request.lender.is_some() {
+            return Err(ErrorCode::AlreadyFunded.into());
+        }
+
+        Ok(())
+    }
+
+
+
+
 }
