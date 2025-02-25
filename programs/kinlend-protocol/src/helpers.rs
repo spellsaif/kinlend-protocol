d3b4ce6734357c51d720dc781877e3532df5be2e
spellsaif <this.saif.email@gmail.com>
2025-02-25 13:26:12 +0530

    added: helper functions

---
+++ programs/kinlend-protocol/src/helpers.rs
@@ -0,0 +1,35 @@
+use anchor_lang::prelude::*;
+
+use crate::errors::ErrorCode;
+
+
+pub fn check_deadline(deadline: i64) -> Result<()> {
+    let clock = Clock::get()?;
+
+    if clock.unix_timestamp > deadline {
+        return Err(ErrorCode::RepaymentTimeExpired.into());
+    }
+
+
+    Ok(())
+}
+
+
+pub fn check_right_borrower(borrower: Pubkey, loan_request_borrower: Pubkey) -> Result<()> {
+
+    if borrower != loan_request_borrower {
+        return Err(ErrorCode::NotRightBorrower.into());
+    }
+
+    Ok(())
+}
+
+
+pub fn check_usdc_mint_address(config_usdc_mint:Pubkey, usdc_mint:Pubkey) -> Result<()> {
+
+    if config_usdc_mint != usdc_mint {
+        return Err(ErrorCode::IncorrectUsdcMintAddress.into());
+    }
+
+    Ok(())
+}
