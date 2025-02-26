3f161c287ceacd4c9352bb2008522fe954e113f3
spellsaif <this.saif.email@gmail.com>
2025-02-26 16:52:55 +0530

    implemented: update_config

--- programs/kinlend-protocol/src/contexts/update_config.rs
+++ programs/kinlend-protocol/src/contexts/update_config.rs
@@ -2,6 +2,7 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::Mint;
 
 use crate::state::ConfigState;
+use crate::errors::ErrorCode;
 
 #[derive(Accounts)]
 pub struct UpdateConfig<'info> {
@@ -18,4 +19,26 @@ pub struct UpdateConfig<'info> {
     pub new_usdc_mint: Account<'info, Mint>,
     
     pub system_program: Program<'info, System>,
+}
+
+impl<'info> UpdateConfig<'info> {
+    pub fn update_config(&mut self) -> Result<()> {
+        //checking if the signer is admin
+        let signer = self.admin.key();
+        self.check_is_admin(signer)?;
+
+        //update config
+        let new_usdc_mint_address = self.new_usdc_mint.key();
+        self.config.usdc_mint = new_usdc_mint_address;
+
+        Ok(())
+    }
+
+    pub fn check_is_admin(&self, signer_key: Pubkey) -> Result<()> {
+        if signer_key != self.config.authority {
+            return Err(ErrorCode::NotAdmin.into());
+        }
+
+        Ok(())
+    }
 }
